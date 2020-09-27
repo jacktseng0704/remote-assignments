f22fdbc3dd8da83c9717d692eae6d7e17c8a03ea
Jack Tseng <b99901190@gmail.com>
2020-09-27 23:50:28 +0800

    Week-3 Assignment2,3,4

--- week-3/app.js
+++ week-3/app.js
@@ -1,6 +1,6 @@
-/* ====================
-       Assignment 1 
-===================== */
+/* =========================================
+       Assignment 1: Your First Web Server
+============================================ */
 
 const express = require('express');
 
@@ -14,3 +14,72 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
+
+
+/* ===================================================
+       Assignment 2: Build Backend API for Front-End
+====================================================== */
+//add positive numbers
+function addNum(number){
+    let sumNum = 0;
+    for(let i = 1; i <= number; i++){
+        sumNum += i;
+    }
+    return sumNum;
+}
+
+function isNormalInteger(str) {
+    return /^([1-9]\d*)$/.test(str);
+}
+
+
+app.get('/getData', (req, res) => {
+    let parameter = req.query.number;
+
+    if (!parameter) {
+        res.send('<h1>Lack Parameter</h1>');
+    } else if (isNormalInteger(parameter)) {
+        res.send(`<h1>Result: ${addNum(Number(parameter))}</h1>`);
+    } else {
+        res.send('<h1>Wrong Parameter</h1>');
+    }
+});
+
+
+
+/* ===================================================
+       Assignment 3: Connect to Backend API by AJAX
+====================================================== */
+app.use(express.static('public'));
+
+
+/* ===============================
+       Assignment 4: HTTP Cookie
+================================= */
+
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+app.get('/myName', (req, res) => {
+    
+    if (req.cookies.username) {
+        res.send(`<h1>${req.cookies.username}</h1>`);
+    } else {
+        res.sendFile(__dirname + '/public/user-form.html');
+    }
+});
+
+app.get('/trackName', (req, res)=>{
+    let name = req.query.name
+    res.cookie('username', name);
+    res.redirect('/myName');
+});
+
+app.get('/removeCookie', (req, res)=>{
+    res.clearCookie('username');
+    res.send('Cookie removed');
+});
+
+
+
